eaf0389f7d44579801a7ecd8d5fe0c717a30bffc
Valerian Hall-Chen <valerian.chen@gmail.com>
2022-09-22 06:43:33 +0100

    Minor formatting changes

--- Scotty_main_sweep_DIII-D_240_188839.py
+++ Scotty_main_sweep_DIII-D_240_188839.py
@@ -16,13 +16,14 @@ from Scotty_init_bruv import get_parameters_for_Scotty
 pol_launch_angles = np.array([-11.4])
 tor_launch_angles = np.linspace(-4.0,9.0,131)
 # launch_freqs_GHz = np.array([55.0,57.5,60.0,62.5,65.0,67.5,70.0,72.5,75.0])
-launch_freqs_GHz = np.array([52.5,65.0,77.5])
+launch_freqs_GHz = np.array([52.5,77.5])
 
 
 equil_time = 1900.0
 
 
-
+counter=0
+total_simulations=len(pol_launch_angles)*len(tor_launch_angles)*len(launch_freqs_GHz)
 for ii, launch_freq_GHz in enumerate(launch_freqs_GHz):
     for jj, pol_launch_angle in enumerate(pol_launch_angles):
         for kk, tor_launch_angle in enumerate(tor_launch_angles):
@@ -43,14 +44,14 @@ for ii, launch_freq_GHz in enumerate(launch_freqs_GHz):
             kwargs_dict['poloidal_flux_enter'] = 1.44
             kwargs_dict['input_filename_suffix'] = '_188839_1900ms'
             # kwargs_dict['output_path'] = os.path.dirname(os.path.abspath(__file__)) + '\\Output\\'
-            kwargs_dict['output_path'] = 'D:\\Dropbox\\VHChen2021\\Data - Scotty\\Run 11\\'
+            kwargs_dict['output_path'] = 'D:\\Dropbox\\VHChen2021\\Data - Scotty\\Run 28\\'
 
             kwargs_dict['delta_R'] = -0.00001
             kwargs_dict['delta_Z'] = -0.00001
             kwargs_dict['delta_K_R'] = 0.1
             kwargs_dict['delta_K_zeta'] = 0.1
             kwargs_dict['delta_K_Z'] = 0.1
-            kwargs_dict['interp_smoothing'] = 2.0
+            kwargs_dict['interp_smoothing'] = 0.0
             kwargs_dict['len_tau'] = 1002
             kwargs_dict['rtol'] = 1e-4
             kwargs_dict['atol'] = 1e-7
@@ -74,9 +75,12 @@ for ii, launch_freq_GHz in enumerate(launch_freqs_GHz):
             else:
                 kwargs_dict['verbose_output_flag'] = False        
 
-            # an_output = kwargs_dict['output_path'] + 'data_output' + kwargs_dict['output_filename_suffix'] + '.npz'
-            # if os.path.exists(an_output):
-            #     continue
-            # else:   
-            #     beam_me_up(**args_dict, **kwargs_dict) 
-            beam_me_up(**args_dict, **kwargs_dict) 
+            counter=counter+1
+            print('simulation ', counter, 'of', total_simulations)
+
+            an_output = kwargs_dict['output_path'] + 'data_output' + kwargs_dict['output_filename_suffix'] + '.npz'
+            if os.path.exists(an_output):
+                continue
+            else:   
+                beam_me_up(**args_dict, **kwargs_dict) 
+            # beam_me_up(**args_dict, **kwargs_dict) 
