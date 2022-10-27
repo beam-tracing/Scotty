5f6402446cdaa2c6b0a62f20a5d49b5c1a54c7e6
Peter Hill <peter.hill@york.ac.uk>
2022-10-27 16:27:46 +0100

    Add test for `find_nearest`

--- tests/test_fun_general.py
+++ tests/test_fun_general.py
@@ -1,4 +1,4 @@
-from scotty.fun_general import freq_GHz_to_wavenumber
+from scotty.fun_general import freq_GHz_to_wavenumber, find_nearest
 
 from scipy.constants import c as speed_of_light
 import numpy as np
@@ -6,3 +6,11 @@ import numpy as np
 
 def test_freq_GHz_to_wavenumber():
     assert np.isclose(freq_GHz_to_wavenumber(1.0 / (2 * np.pi)), 1e9 / speed_of_light)
+
+
+def test_find_nearest():
+    data = [1, 3, 5, 7, 9]
+    assert find_nearest(data, 5.5) == 2
+    assert find_nearest(data, 6.5) == 3
+    assert find_nearest(data, 0.5) == 0
+    assert find_nearest(data, 10) == 4
