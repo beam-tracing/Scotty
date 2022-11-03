4551d16aa0682ed34318b91448b7ba7314623ba6
Peter Hill <peter.hill@york.ac.uk>
2022-11-03 15:33:37 +0000

    Add type alias for float array-like
    The generic `ArrayLike` version can be any dtype, which causes issues
    with mypy

--- scotty/typing.py
+++ scotty/typing.py
@@ -1,5 +1,7 @@
 from os import PathLike as os_PathLike
 from typing import Union
+import numpy as np
+from numpy.typing import NDArray
 
-
+ArrayLike = Union[float, NDArray[np.float64]]
 PathLike = Union[os_PathLike, str]
