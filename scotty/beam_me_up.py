Bug fix: wrong sign on B_Z for analytical equilibrium

scotty/beam_me_up.py
@@ -538,7 +538,7 @@ def beam_me_up(
 
         def find_B_p(q_R, q_Z, R_axis, minor_radius_a, B_p_a):
             B_p = B_p_a * (np.sqrt((q_R - R_axis) ** 2 + q_Z**2) / minor_radius_a)
-            return B_p
+            return -B_p
 
         def find_B_R(
             q_R, q_Z, R_axis=R_axis, minor_radius_a=minor_radius_a, B_p_a=B_p_a
@@ -556,7 +556,7 @@ def beam_me_up(
         ):
             B_p = find_B_p(q_R, q_Z, R_axis, minor_radius_a, B_p_a)
             B_Z = B_p * ((q_R - R_axis) / np.sqrt((q_R - R_axis) ** 2 + q_Z**2))
-            return B_Z
+            return -B_Z
 
         def interp_poloidal_flux(
             q_R, q_Z, R_axis=R_axis, minor_radius_a=minor_radius_a, grid=None

tests/test_simple_golden.py
@@ -8,29 +8,70 @@ from numpy.testing import assert_allclose
 TAU_EXPECTED = np.array(
     [
         0.0,
-        62.69833027,
-        125.39666054,
-        188.09499082,
-        250.79332109,
-        282.12565681,
-        313.49165136,
-        376.18998163,
-        438.8883119,
-        501.58664217,
+        62.647098,
+        125.294195,
+        187.941293,
+        250.58839,
+        281.93231,
+        313.235488,
+        375.882585,
+        438.529683,
+        501.176781,
     ]
 )
+
 B_EXPECTED = np.array(
     [
-        0.75893835,
-        0.79481524,
-        0.82660747,
-        0.85400248,
-        0.87701708,
-        0.88693792,
-        0.89586574,
-        0.91087338,
-        0.92241292,
-        0.93088217,
+        0.758938,
+        0.794796,
+        0.82659,
+        0.85399,
+        0.877001,
+        0.886924,
+        0.895815,
+        0.910728,
+        0.922128,
+        0.930421,
+    ]
+)
+
+PSI_START_EXPECTED = np.array(
+    [
+        [
+            -2.47359641e03 + 1.19557798e01j,
+            1.40800267e-02 + 1.44252978e-02j,
+            3.40526964e02 - 1.13752564e02j,
+        ],
+        [
+            1.40800267e-02 + 1.44252978e-02j,
+            4.24589207e03 + 4.35001002e03j,
+            -2.22523411e-03 - 2.27980139e-03j,
+        ],
+        [
+            3.40526964e02 - 1.13752564e02j,
+            -2.22523411e-03 - 2.27980139e-03j,
+            4.25775858e02 + 1.08229209e03j,
+        ],
+    ]
+)
+
+PSI_FINAL_EXPECTED = np.array(
+    [
+        [
+            -1610.67883254 + 1.27655263e04j,
+            -49.98203756 + 3.42882509e01j,
+            -553.16605173 - 1.41526821e03j,
+        ],
+        [
+            -49.98203756 + 3.42882509e01j,
+            3549.99540711 + 1.91633120e03j,
+            32.60030345 + 1.07767585e01j,
+        ],
+        [
+            -553.16605173 - 1.41526821e03j,
+            32.60030345 + 1.07767585e01j,
+            2466.57382678 + 1.57149892e02j,
+        ],
     ]
 )
 
@@ -54,6 +95,8 @@ def test_simple_check(tmp_path):
 
     assert_allclose(output["tau_array"], TAU_EXPECTED, rtol=1e-5)
     assert_allclose(output["B_magnitude"], B_EXPECTED, rtol=1e-5)
+    assert_allclose(output["Psi_3D_output"][0, ...], PSI_START_EXPECTED, rtol=1e-2)
+    assert_allclose(output["Psi_3D_output"][-1, ...], PSI_FINAL_EXPECTED, rtol=1e-2)
 
 
 def test_simple_check_density_fit_file(tmp_path):
@@ -88,3 +131,7 @@ def test_simple_check_density_fit_file(tmp_path):
 
     assert_allclose(output["tau_array"], TAU_EXPECTED, rtol=1e-5)
     assert_allclose(output["B_magnitude"], B_EXPECTED, rtol=1e-5)
+    assert_allclose(output["Psi_3D_output"][0, ...], PSI_START_EXPECTED, rtol=1e-2)
+    # Slightly larger tolerance here, likely due to floating-point
+    # precision of file-based input
+    assert_allclose(output["Psi_3D_output"][-1, ...], PSI_FINAL_EXPECTED, rtol=1.5e-2)