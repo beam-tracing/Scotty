b4c1a32f4f1074289a4a676287bc88de4f07ba6e
Peter Hill <peter.hill@york.ac.uk>
2022-11-07 10:23:50 +0000

    Set `None` as default for `density_fit_method`
    This aids backwards compatibility

--- scotty/beam_me_up.py
+++ scotty/beam_me_up.py
@@ -203,8 +203,8 @@ def beam_me_up(
     ## For launching within the plasma
     plasmaLaunch_K=np.zeros(3),
     plasmaLaunch_Psi_3D_lab_Cartesian=np.zeros([3, 3]),
-    density_fit_parameters=None,
-    density_fit_method: Union[str, DensityFitLike] = "smoothing-spline-file",
+    density_fit_parameters: Optional[Sequence] = None,
+    density_fit_method: Optional[Union[str, DensityFitLike]] = None,
     ## For circular flux surfaces
     B_T_axis=None,
     B_p_a=None,
