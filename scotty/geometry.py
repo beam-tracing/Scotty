f16593d40014352121e5515b8991a665ec4492fd
Peter Hill <peter.hill@york.ac.uk>
2022-12-05 16:20:27 +0000

    Tweak type hints for magnetic field methods

--- scotty/geometry.py
+++ scotty/geometry.py
@@ -7,20 +7,20 @@ import numpy.typing as npt
 from scipy.interpolate import RectBivariateSpline, UnivariateSpline
 
 from scotty.fun_CFD import find_dpolflux_dR, find_dpolflux_dZ
-from scotty.typing import ArrayLike
+from scotty.typing import ArrayLike, FloatArray
 
 
 class MagneticGeometry:
-    def B_R(self, q_R: ArrayLike, q_Z: ArrayLike) -> ArrayLike:
+    def B_R(self, q_R: ArrayLike, q_Z: ArrayLike) -> FloatArray:
         raise NotImplementedError
 
-    def B_T(self, q_R: ArrayLike, q_Z: ArrayLike) -> ArrayLike:
+    def B_T(self, q_R: ArrayLike, q_Z: ArrayLike) -> FloatArray:
         raise NotImplementedError
 
-    def B_Z(self, q_R: ArrayLike, q_Z: ArrayLike) -> ArrayLike:
+    def B_Z(self, q_R: ArrayLike, q_Z: ArrayLike) -> FloatArray:
         raise NotImplementedError
 
-    def poloidal_flux(self, q_R: ArrayLike, q_Z: ArrayLike) -> ArrayLike:
+    def poloidal_flux(self, q_R: ArrayLike, q_Z: ArrayLike) -> FloatArray:
         raise NotImplementedError
 
 
@@ -64,19 +64,19 @@ class CircularCrossSection(MagneticGeometry):
             *np.meshgrid(self.data_R_coord, self.data_Z_coord, indexing="ij")
         )
 
-    def rho(self, q_R: ArrayLike, q_Z: ArrayLike) -> ArrayLike:
+    def rho(self, q_R: ArrayLike, q_Z: ArrayLike) -> FloatArray:
         q_R, q_Z = np.asfarray(q_R), np.asfarray(q_Z)
         return np.sqrt((q_R - self.R_axis) ** 2 + q_Z**2)
 
-    def B_R(self, q_R: ArrayLike, q_Z: ArrayLike) -> ArrayLike:
+    def B_R(self, q_R: ArrayLike, q_Z: ArrayLike) -> FloatArray:
         q_R, q_Z = np.asfarray(q_R), np.asfarray(q_Z)
         return self.B_p_a * q_Z / (q_R * self.minor_radius_a * self.rho(q_R, q_Z))
 
-    def B_T(self, q_R: ArrayLike, q_Z: ArrayLike) -> ArrayLike:
+    def B_T(self, q_R: ArrayLike, q_Z: ArrayLike) -> FloatArray:
         q_R, q_Z = np.asfarray(q_R), np.asfarray(q_Z)
         return self.B_T_axis * (self.R_axis / q_R)
 
-    def B_Z(self, q_R: ArrayLike, q_Z: ArrayLike) -> ArrayLike:
+    def B_Z(self, q_R: ArrayLike, q_Z: ArrayLike) -> FloatArray:
         q_R, q_Z = np.asfarray(q_R), np.asfarray(q_Z)
         return (
             -self.B_p_a
@@ -84,7 +84,7 @@ class CircularCrossSection(MagneticGeometry):
             / (q_R * self.minor_radius_a * self.rho(q_R, q_Z))
         )
 
-    def poloidal_flux(self, q_R: ArrayLike, q_Z: ArrayLike) -> ArrayLike:
+    def poloidal_flux(self, q_R: ArrayLike, q_Z: ArrayLike) -> FloatArray:
         q_R, q_Z = np.asfarray(q_R), np.asfarray(q_Z)
         return self.rho(q_R, q_Z) / self.minor_radius_a
 
@@ -96,22 +96,22 @@ class CurvySlab(MagneticGeometry):
         self.B_T_axis = B_T_axis
         self.R_axis = R_axis
 
-    def B_R(self, q_R: ArrayLike, q_Z: ArrayLike) -> ArrayLike:
+    def B_R(self, q_R: ArrayLike, q_Z: ArrayLike) -> FloatArray:
         q_R, q_Z = np.asfarray(q_R), np.asfarray(q_Z)
         return np.zeros_like(q_R)
 
-    def B_T(self, q_R: ArrayLike, q_Z: ArrayLike) -> ArrayLike:
+    def B_T(self, q_R: ArrayLike, q_Z: ArrayLike) -> FloatArray:
         q_R, q_Z = np.asfarray(q_R), np.asfarray(q_Z)
         return self.B_T_axis * self.R_axis / q_R
 
-    def B_Z(self, q_R: ArrayLike, q_Z: ArrayLike) -> ArrayLike:
+    def B_Z(self, q_R: ArrayLike, q_Z: ArrayLike) -> FloatArray:
         q_R, q_Z = np.asfarray(q_R), np.asfarray(q_Z)
         return np.zeros_like(q_R)
 
 
 def _make_rect_spline(
     R_grid, Z_grid, array, interp_order: int, interp_smoothing: int
-) -> Callable[[ArrayLike, ArrayLike], ArrayLike]:
+) -> Callable[[ArrayLike, ArrayLike], FloatArray]:
     spline = RectBivariateSpline(
         R_grid,
         Z_grid,
@@ -175,16 +175,16 @@ class InterpolatedField(MagneticGeometry):
         self.data_Z_coord = Z_grid
         self.poloidalFlux_grid = psi
 
-    def B_R(self, q_R: ArrayLike, q_Z: ArrayLike) -> ArrayLike:
+    def B_R(self, q_R: ArrayLike, q_Z: ArrayLike) -> FloatArray:
         return self._interp_B_R(q_R, q_Z)
 
-    def B_T(self, q_R: ArrayLike, q_Z: ArrayLike) -> ArrayLike:
+    def B_T(self, q_R: ArrayLike, q_Z: ArrayLike) -> FloatArray:
         return self._interp_B_T(q_R, q_Z)
 
-    def B_Z(self, q_R: ArrayLike, q_Z: ArrayLike) -> ArrayLike:
+    def B_Z(self, q_R: ArrayLike, q_Z: ArrayLike) -> FloatArray:
         return self._interp_B_Z(q_R, q_Z)
 
-    def poloidal_flux(self, q_R: ArrayLike, q_Z: ArrayLike) -> ArrayLike:
+    def poloidal_flux(self, q_R: ArrayLike, q_Z: ArrayLike) -> FloatArray:
         return self._interp_poloidal_flux(q_R, q_Z)
 
 
@@ -229,21 +229,21 @@ class EFITField(MagneticGeometry):
             check_finite=False,
         )
 
-    def B_R(self, q_R, q_Z):
+    def B_R(self, q_R: ArrayLike, q_Z: ArrayLike) -> FloatArray:
         dpolflux_dZ = find_dpolflux_dZ(
             q_R, q_Z, self.delta_Z, self._interp_poloidal_flux
         )
         return -dpolflux_dZ * self.poloidal_flux_gradient / q_R
 
-    def B_T(self, q_R, q_Z):
+    def B_T(self, q_R: ArrayLike, q_Z: ArrayLike) -> FloatArray:
         polflux = self._interp_poloidal_flux(q_R, q_Z)
         return self._interp_rBphi(polflux) / q_R
 
-    def B_Z(self, q_R, q_Z):
+    def B_Z(self, q_R: ArrayLike, q_Z: ArrayLike) -> FloatArray:
         dpolflux_dR = find_dpolflux_dR(
             q_R, q_Z, self.delta_R, self._interp_poloidal_flux
         )
         return dpolflux_dR * self.poloidal_flux_gradient / q_R
 
-    def poloidal_flux(self, q_R, q_Z):
+    def poloidal_flux(self, q_R: ArrayLike, q_Z: ArrayLike) -> FloatArray:
         return self._interp_poloidal_flux(q_R, q_Z)
--- scotty/typing.py
+++ scotty/typing.py
@@ -3,5 +3,6 @@ from typing import Union
 import numpy as np
 from numpy.typing import NDArray
 
-ArrayLike = Union[float, NDArray[np.float64]]
+FloatArray = NDArray[np.float64]
+ArrayLike = Union[float, FloatArray]
 PathLike = Union[os_PathLike, str]
