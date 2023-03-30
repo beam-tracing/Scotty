86af1f614e3b023e318994285a62549f46e9ea24
Peter Hill <peter.hill@york.ac.uk>
2023-03-30 14:31:02 +0100

    Fix docs for check_input/output

--- scotty/check_input.py
+++ scotty/check_input.py
@@ -1,18 +1,18 @@
 # -*- coding: utf-8 -*-
-"""
-Created on 08 November 2022
+"""Created on 08 November 2022
 
 @author: VH Hall-Chen
 Valerian Hongjie Hall-Chen
 valerian.chen@gmail.com
 
-Checks input arguments of beam_me_up
+Checks input arguments of `beam_me_up <scotty.beam_me_up.beam_me_up>`
+
 """
 
 from __future__ import annotations
 
 
-def check_mode_flag(mode_flag):
+def check_mode_flag(mode_flag: int) -> None:
     """Mode flag should be either -1 (X-mode) or 1 (O-mode)"""
 
     if mode_flag not in [-1, 1]:
@@ -20,10 +20,6 @@ def check_mode_flag(mode_flag):
             f"Bad value for `mode_flag`! Expected either 1 or -1, got '{mode_flag}'"
         )
 
-    return None
 
-
-def check_input(mode_flag):
+def check_input(mode_flag: int) -> None:
     check_mode_flag(mode_flag)
-
-    return None
--- scotty/check_output.py
+++ scotty/check_output.py
@@ -1,59 +1,59 @@
 # -*- coding: utf-8 -*-
-"""
-Created on 08 November 2022
+"""Created on 08 November 2022
 
 @author: VH Hall-Chen
 Valerian Hongjie Hall-Chen
 valerian.chen@gmail.com
 
-Checks various quantities that beam_me_up calculates
+Checks various quantities that `beam_me_up
+<scotty.beam_me_up.beam_me_up>` calculates
+
 """
 
 from __future__ import annotations
 import numpy as np
-import warnings
+from warnings import warn
+
+from scotty.typing import FloatArray
 
 
-
-def check_H_output(H_output):
+def check_H_output(H_output: FloatArray) -> None:
     """H_output should be zero along the entire ray.
 
     If not close to zero, that means numerical errors are large
     """
     tol = 1e-2
 
-    if max(H_output) > tol:
-        warnings.warn(
-            (
-                f"WARNING: `H_output` is too large! `max H_output` = '{max(H_output)}' but 'tol' = '{tol}'"
-            )
+    max_H = max(H_output)
+    if max_H > tol:
+        warn(
+            f"WARNING: `H_output` is too large! `max H_output = {max_H}` but `tol = {tol}`"
         )
-    elif max(H_output) > 1.0:
-        raise ValueError(
-            f"`H_output` is unphysically large! `max H_output` = '{max(H_output)}'"
-        )
-
-    return None
+    elif max_H > 1.0:
+        raise ValueError(f"`H_output` is unphysically large! `max H_output = {max_H}`")
 
 
-def check_Psi(Psi_xg_output, Psi_yg_output, Psi_gg_output):
-    # By definition, we need
-    # Im(\Psi \cdot \hat{\mathbf{g}}) = 0
-    # Re(\Psi \cdot \hat{\mathbf{g}}) = \nabla_K H
+def check_Psi(
+    Psi_xg_output: FloatArray, Psi_yg_output: FloatArray, Psi_gg_output: FloatArray
+) -> None:
+    r"""
+    By definition, we need:
 
+    .. math::
+
+        \begin{align}
+        Im(\Psi \cdot \hat{\mathbf{g}}) &= 0 \\
+        Re(\Psi \cdot \hat{\mathbf{g}}) &= \nabla_K H
+        \end{align}
+    """
     tol = 1e-3
 
-    if max(np.imag(Psi_xg_output)) > tol:
-        warnings.warn(
-            (
-                f"WARNING: `H_output` is too large! `max Psi_xg` = '{max(Psi_xg_output)}' but 'tol' = '{tol}'"
-            )
+    max_Psi = max(np.imag(Psi_xg_output))
+    if max_Psi > tol:
+        warn(
+            f"WARNING: `Im(Psi_xg)` is too large! `max Im(Psi_xg) = {max_Psi}` but `tol = {tol}`"
         )
 
-    return None
 
-
-def check_output(H_output):
+def check_output(H_output: FloatArray) -> None:
     check_H_output(H_output)
-
-    return None
