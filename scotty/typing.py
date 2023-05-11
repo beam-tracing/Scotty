fc5f3450fd0278ba70d148a87c66ee275ec9e602
ZedThree <ZedThree@users.noreply.github.com>
2023-05-11 10:31:35 +0000

    Apply black changes

--- scotty/typing.py
+++ scotty/typing.py
@@ -4,11 +4,13 @@
 from os import PathLike as os_PathLike
 from typing import Union
 import numpy as np
+
 try:
     from numpy.typing import NDArray
+
     FloatArray = NDArray[np.float64]
 except ImportError:
-    FloatArray = np.ndarray     # type: ignore
+    FloatArray = np.ndarray  # type: ignore
 
 ArrayLike = Union[float, FloatArray]
 PathLike = Union[os_PathLike, str]
